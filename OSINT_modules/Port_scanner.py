25649dc4a87a31abfe1cfe6c7becef1daae3dd3a
TNB <lyubog2@mail.ru>
2022-06-12 16:30:51 +0500

    Scanning open ports of domain

--- OSINT_modules/Port_scanner.py
+++ OSINT_modules/Port_scanner.py
@@ -2,27 +2,23 @@ import socket
 import sys
 import threading
 
-from Subdomain_scaner import *
-
 DATA = []
 
 
 def port_scanner(target):
     target = target
-    open_ports = [[target], []]
+    open_ports = [target, []]
     try:
         # for scanning all open ports put 65536 instead of 1024 in port (mach longer output)
-        for port in range(1, 1024):
+        for port in range(1, 27):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(1)
             result = s.connect_ex((target, port))
             if result == 0:
-                print(f'Port {port} is open\n')
                 open_ports[1].append(port)
             s.close()
-        print(f'Open ports of {target} is {open_ports[1]}\n')
         DATA.append(open_ports)
-        # return open_ports
+        return open_ports
     except KeyboardInterrupt:
         print('\n Exiting Program !!!!')
         sys.exit()
@@ -34,8 +30,7 @@ def port_scanner(target):
         sys.exit()
 
 
-# В main кидается список доменов
-def main(domain_list):
+def scan_threading(domain_list):
     domain_list = domain_list
     threads = list()
     for item in domain_list:
@@ -47,6 +42,9 @@ def main(domain_list):
     print(DATA)
 
 
+def main():
+    pass
+
+
 if __name__ == '__main__':
-    d_list = parse_collection(read_data_from_json('info'))
-    main(d_list)
+    main()
